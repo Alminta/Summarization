176250b7fe19244ddab18903941661a87b6613b5
Alminta <almintars@gmail.com>
2018-11-05 16:17:41 +0100

    kermit

--- main.py
+++ main.py
@@ -1,10 +1,75 @@
 from funcs import generate
+from data_generator import generate
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
-vocab=['a','b','c','d','e','f']
+from data_generator import generate
+
+#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = "cpu"
+print("Device in use:", device)
+
+NUM_INPUTS = 27 #No. of possible characters
+NUM_OUTPUTS = 11  # (0-9 + '#')
+
+### Hyperparameters and general configs
+MAX_SEQ_LEN = 8
+MIN_SEQ_LEN = 5
+BATCH_SIZE = 8
+TRAINING_SIZE = 8000
+LEARNING_RATE = 0.001
+
+# Hidden size of enc and dec need to be equal if last hidden of encoder becomes init hidden of decoder
+# Otherwise we would need e.g. a linear layer to map to a space with the correct dimension
+NUM_UNITS_ENC = NUM_UNITS_DEC = 48
+TEST_SIZE = 200
+EPOCHS = 10
+TEACHER_FORCING = True
+
+assert TRAINING_SIZE % BATCH_SIZE == 0
 
 
-a,b=generate(10,100,vocab,0.2)
+encoder = EncoderRNN(NUM_INPUTS, NUM_UNITS_ENC).to(device)
+decoder = DecoderRNN(NUM_UNITS_DEC, NUM_OUTPUTS).to(device)
+enc_optimizer = optim.RMSprop(encoder.parameters(), lr=LEARNING_RATE,weight_decay=1e-6,momentum=0.5)
+dec_optimizer = optim.RMSprop(decoder.parameters(), lr=LEARNING_RATE,weight_decay=1e-6,momentum=0.5)
+criterion = nn.NLLLoss()
 
+# Get training set
+inputs, _, targets_in, targets, targets_seqlen, _, _, _, text_targ = generate(TRAINING_SIZE, min_len=MIN_SEQ_LEN, max_len=MAX_SEQ_LEN)
+max_target_len = max(targets_seqlen)
+inputs = torch.tensor(inputs).long()
+targets = torch.tensor(targets).long()
+targets_in = torch.tensor(targets_in).long()
+unique_text_targets = set(text_targ)
 
-print(a)
-print(b)
+# Get validation set
+val_inputs, _, val_targets_in, val_targets, val_targets_seqlen, _, val_text_in, _, val_text_targ = \
+    generate(TEST_SIZE, min_len=MIN_SEQ_LEN, max_len=MAX_SEQ_LEN, invalid_set=unique_text_targets)
+val_inputs = torch.tensor(val_inputs)
+val_targets = torch.tensor(val_targets)
+val_targets_in = torch.tensor(val_targets_in)
+max_val_target_len = max(val_targets_seqlen)
+test(encoder, decoder, val_inputs, val_targets, val_targets_in, criterion, max_val_target_len)
+
+# Split training set in batches
+inputs = [inputs[i * BATCH_SIZE: (i + 1) * BATCH_SIZE] for i in range(TRAINING_SIZE // BATCH_SIZE)]
+targets = [targets[i * BATCH_SIZE: (i + 1) * BATCH_SIZE] for i in range(TRAINING_SIZE // BATCH_SIZE)]
+targets_in = [targets_in[i * BATCH_SIZE: (i + 1) * BATCH_SIZE] for i in range(TRAINING_SIZE // BATCH_SIZE)]
+
+# Quick and dirty - just loop over training set without reshuffling
+for epoch in range(1, EPOCHS + 1):
+    train(encoder, decoder, inputs, targets, targets_in, criterion, enc_optimizer, dec_optimizer, epoch, max_target_len)
+    _, loss, accuracy = test(encoder, decoder, val_inputs, val_targets, val_targets_in, criterion, max_val_target_len)
+    print('\nTest set: Average loss: {:.4f} \tAccuracy: {:.3f}%\n'.format(loss, accuracy.item()*100.))
+
+    # Show examples
+    print("Examples: prediction | input")
+    out, _, _ = test(encoder, decoder, val_inputs[:10], val_targets[:10], val_targets_in[:10], criterion, max_val_target_len)
+    pred = get_pred(out)
+    pred_text = [numbers_to_text(sample) for sample in pred]
+    for i in range(10):
+        print(pred_text[i], "\t", val_text_in[i])
+    print()
