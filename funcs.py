839e2c73f76a88cdb34b1767f4a5aa6b54dcfc2e
Rasmus <ybionz@gmail.com>
2018-11-19 14:36:52 +0100

    ybionz

--- funcs.py
+++ funcs.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.nn.parameter import Parameter
 
 device = "cpu"
 SEQ_NUM = 800
@@ -156,6 +157,25 @@ class DecoderRNN(nn.Module):
         self.out = nn.Linear(self.hidden_size, self.output_size)
         rnn = nn.LSTM
         self.rnn = rnn(self.hidden_size, self.hidden_size, 1, batch_first=True)
+        
+
+        # Attention
+
+        #.W_2 = Parameter(init.kaiming_normal_(torch.Tensor(num_output, num_hidden))) #template
+
+        self.v_a = Parameter(init.kaiming_normal_(torch.Tensor(hidden_size, 1)))
+
+        self.W_h = Parameter(init.kaiming_normal_(torch.Tensor(hidden_size, hidden_size)))
+
+        self.W_s = Parameter(init.kaiming_normal_(torch.Tensor(hidden_size, hidden_size)))
+
+        #self.w_c = Parameter(init.kaiming_normal_(torch.Tensor(num_output, num_hidden))) need to be defined
+
+        self.b_alpha = Parameter(init.constant_(torch.Tensor(hidden_size),0))
+
+
+
+
 
     def forward(self, inputs, hidden, output_len, cn, teacher_forcing=False):
         # Input shape: [batch, output_len]
@@ -167,6 +187,9 @@ class DecoderRNN(nn.Module):
             dec_input = inputs
             embedded = self.embedding(dec_input)   # shape [batch, output_len, hidden_dim]
             out, (hidden, cn) = self.rnn(embedded, (hidden,cn))
+            
+
+
             out = self.out(out)  # linear layer, out has now shape [batch, output_len, output_size]
             output = F.log_softmax(out, -1)
         else:
@@ -208,11 +231,9 @@ def forward_pass(encoder, decoder, x, t, t_in, criterion, max_t_len, teacher_for
     enc_out, enc_h, cn= encoder(x, enc_h,cn)
     #print(enc_out.size())
     dec_h = enc_h  # Init hidden state of decoder as hidden state of encoder
-    print(enc_out.shape)
     dec_input = t_in
     out = decoder(dec_input, dec_h, max_t_len, cn, teacher_forcing)
     out = out.permute(0, 2, 1)
-
     # Shape: [batch_size x num_classes x out_sequence_len], with second dim containing log probabilities
 
     loss = criterion(out, t)
@@ -278,163 +299,3 @@ def get_pred(log_probs):
     :return:
     """
     return torch.argmax(log_probs, dim=1)
-
-
-"""
-def attention_decoder(attention_input, attention_lengths, initial_state, target_input,
-                      target_input_lengths, num_attn_units, embeddings, W_out, b_out,
-                      name='decoder', swap=False):
-                      """
-"""
-    Decoder with attention.
-    Note that the number of units in the attention decoder must always
-    be equal to the size of the initial state/attention input.
-    Keyword arguments:
-        attention_input:    the input to put attention on. expected dims: [batch_size, attention_length, attention_dims]
-        initial_state:      The initial state for the decoder RNN.
-        target_input:       The target to replicate. Expected: [batch_size, max_target_sequence_len, embedding_dims]
-        num_attn_units:     Number of units in the alignment layer that produces the context vectors.
-"""
-"""
-    with tf.variable_scope(name):
-        target_dims = target_input.get_shape()[2]
-        attention_dims = attention_input.get_shape()[2]
-        num_units = attention_dims
-        attn_len = tf.shape(attention_input)[1]
-        max_sequence_length = tf.reduce_max(target_input_lengths)
-
-        weight_initializer = tf.truncated_normal_initializer(stddev=0.1)
-        W_z = tf.get_variable('W_z',
-                              shape=[target_dims+num_units*2, num_units],
-                              initializer=weight_initializer)
-        W_r = tf.get_variable('W_r',
-                              shape=[target_dims+num_units*2, num_units],
-                              initializer=weight_initializer)
-        W_h = tf.get_variable('W_h',
-                              shape=[target_dims+num_units*2, num_units],
-                              initializer=weight_initializer)
-        b_z = tf.get_variable('b_z',
-                              shape=[num_units],
-                              initializer=tf.constant_initializer(1.0))
-        b_r = tf.get_variable('b_r',
-                              shape=[num_units],
-                              initializer=tf.constant_initializer(1.0))
-        b_h = tf.get_variable('b_h',
-                              shape=[num_units],
-                              initializer=tf.constant_initializer())
-
-        # for attention
-        W_a = tf.get_variable('W_a',
-                              shape=[attention_dims, num_attn_units],
-                              initializer=weight_initializer)
-        U_a = tf.get_variable('U_a',
-                              shape=[1, 1, attention_dims, num_attn_units],
-                              initializer=weight_initializer)
-        b_a = tf.get_variable('b_a',
-                              shape=[num_attn_units],
-                              initializer=tf.constant_initializer())
-        v_a = tf.get_variable('v_a',
-                              shape=[num_attn_units],
-                              initializer=weight_initializer)
-
-        # TODO: don't use convolutions!
-        # TODO: fix the bias (b_a)
-        hidden = tf.reshape(attention_input, tf.pack([-1, attn_len, 1, attention_dims]))
-        part1 = tf.nn.conv2d(hidden, U_a, [1, 1, 1, 1], "SAME")
-        part1 = tf.squeeze(part1, [2])  # squeeze out the third dimension
-
-        inputs = tf.transpose(target_input, perm=[1, 0, 2])
-        input_ta = tensor_array_ops.TensorArray(tf.float32, size=1, dynamic_size=True)
-        input_ta = input_ta.unpack(inputs)
-
-        def decoder_cond(time, state, output_ta_t, attention_tracker):
-            return tf.less(time, max_sequence_length)
-
-        def decoder_body_builder(feedback=False):
-            def decoder_body(time, old_state, output_ta_t, attention_tracker):
-                if feedback:
-                    def from_previous():
-                        prev_1 = tf.matmul(old_state, W_out) + b_out
-                        return tf.gather(embeddings, tf.argmax(prev_1, 1))
-                    x_t = tf.cond(tf.greater(time, 0), from_previous, lambda: input_ta.read(0))
-                else:
-                    x_t = input_ta.read(time)
-
-                # attention
-                #part2 = tf.matmul(old_state, W_a) + b_a
-                part2 = torch.matmul(old_state, W_a)+ b_a
-                #part2 = tf.expand_dims(part2, 1)
-                part2 = part2.unsqueeze(1)
-                john = part1 + part2
-                #e = tf.reduce_sum(v_a * torch.tanh(john), [2])
-                e = np.sum(v_a * torch.tanh(john), axis=2)
-                #alpha = tf.nn.softmax(e)
-                alpha = F.softmax(e)
-                #alpha = tf.to_float(mask(attention_lengths)) * alpha
-                alpha = mask(attention_lengths).type(torch.FloatTensor) * alpha
-                #alpha = alpha / tf.reduce_sum(alpha, [1], keep_dims=True)
-                alpha = alpha / np.sum(alpha, axis=1)
-                #attention_tracker = attention_tracker.write(time, alpha)
-                # WE DO NOT USE THE ATTENTION TRACKER I GUESS https://i.kym-cdn.com/photos/images/original/001/231/999/ba5.jpg
-
-                #c = tf.reduce_sum(tf.expand_dims(alpha, 2) * tf.squeeze(hidden), [1])
-                c = np.sum(alpha.unsqueeze(2)*hidden.squeeze(),axis=1)
-
-"""
-"""
-                # GRU
-                con = tf.concat(1, [x_t, old_state, c])
-                z = tf.sigmoid(tf.matmul(con, W_z) + b_z)
-                r = tf.sigmoid(tf.matmul(con, W_r) + b_r)
-                con = tf.concat(1, [x_t, r*old_state, c])
-                h = tf.tanh(tf.matmul(con, W_h) + b_h)
-                new_state = (1-z)*h + z*old_state
-                
-                output_ta_t = output_ta_t.write(time, new_state)
-"""
-"""
-                return (time + 1, new_state, output_ta_t, attention_tracker)
-            return decoder_body
-
-
-        output_ta = tensor_array_ops.TensorArray(tf.float32, size=1, dynamic_size=True)
-        attention_tracker = tensor_array_ops.TensorArray(tf.float32, size=1, dynamic_size=True)
-        time = tf.constant(0)
-        loop_vars = [time, initial_state, output_ta, attention_tracker]
-
-        _, state, output_ta, _ = tf.while_loop(decoder_cond,
-                                            decoder_body_builder(),
-                                            loop_vars,
-                                            swap_memory=swap)
-        _, valid_state, valid_output_ta, valid_attention_tracker = tf.while_loop(decoder_cond,
-                                                        decoder_body_builder(feedback=True),
-                                                        loop_vars,
-                                                        swap_memory=swap)
-
-        dec_state = state
-        dec_out = tf.transpose(output_ta.pack(), perm=[1, 0, 2])
-        valid_dec_out = tf.transpose(valid_output_ta.pack(), perm=[1, 0, 2])
-
-        return dec_state, dec_out, valid_dec_out, valid_attention_tracker
-
-
-def mask(sequence_lengths):
-    # based on this SO answer: http://stackoverflow.com/a/34138336/118173
-    #batch_size = tf.shape(sequence_lengths)[0]
-    batch_size = sequence_lengths.shape[0]
-    #max_len = tf.reduce_max(sequence_lengths)
-    max_len = np.max(sequence_lengths)
-    
-    #lengths_transposed = tf.expand_dims(sequence_lengths, 1)
-    lengths_transposed = sequence_lengths.unsqueeze(1)
-    
-    #rng = tf.range(max_len)
-    rng = torch.arange(max_len)
-    
-    #rng_row = tf.expand_dims(rng, 0)
-    rng_row = rng.unsqueeze(0)
-
-    #return tf.less(rng_row, lengths_transposed)
-    return rng_row<lengths_transposed
-
-"""
