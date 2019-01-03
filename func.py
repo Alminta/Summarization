efdc48188e9867554e5a3cdbcb5b76c3f100a521
Alminta <almintars@gmail.com>
2019-01-03 23:28:35 +0100

    Finale

--- func.py
+++ func.py
@@ -660,3 +660,360 @@ def get_pred(log_probs):
     return torch.argmax(log_probs, dim=1)
 
 # junk in the bottom
+
+class EncoderRNNNo(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super().__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(input_size, self.hidden_size)
+        rnn = nn.LSTM
+        self.rnn = rnn(self.hidden_size, self.hidden_size, 1, batch_first=True)
+
+    def forward(self, inputs, hidden,cn):
+        # Input shape [batch, seq_in_len]z
+        inputs = inputs.long()
+
+        # Embedded shape [batch, seq_in_len, embed]
+        embedded = self.embedding(inputs)
+        
+        # Output shape [batch, seq_in_len, embed]
+        # Hidden shape [1, batch, embed], last hidden state of the GRU cell
+        # We will feed this last hidden state into the decoder
+        output, (hidden,cn) = self.rnn(embedded, (hidden,cn))
+        return output, hidden, cn
+
+    def init_hidden(self, batch_size):
+        init = torch.zeros(1, batch_size, self.hidden_size, device=device)
+        cn = torch.zeros(1,batch_size, self.hidden_size, device=device)
+        return init, cn
+
+
+class DecoderRNNNo(nn.Module):
+    def __init__(self, hidden_size, output_size):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+
+        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
+        self.out = nn.Linear(self.hidden_size, self.output_size)
+        rnn = nn.LSTM
+        self.rnn = rnn(self.hidden_size, self.hidden_size, 1, batch_first=True)
+
+    def forward(self, inputs, hidden, output_len, cn, teacher_forcing=False):
+        # Input shape: [batch, output_len]
+        # Hidden shape: [seq_len=1, batch_size, hidden_dim] (the last hidden state of the encoder)
+        #print('c_0: {:}'.format(c_0))
+        #print('teacher_forching{:}'.format(teacher_forcing))
+              
+        if teacher_forcing:
+            dec_input = inputs
+            embedded = self.embedding(dec_input)   # shape [batch, output_len, hidden_dim]
+            out, (hidden, cn) = self.rnn(embedded, (hidden,cn))
+            out = self.out(out)  # linear layer, out has now shape [batch, output_len, output_size]
+            output = F.log_softmax(out, -1)
+        else:
+            # Take the EOS character only, for the whole batch, and unsqueeze so shape is [batch, 1]
+            # This is the first input, then we will use as input the GRU output at the previous time step
+            dec_input = inputs[:, 0].unsqueeze(1)
+
+            output = []
+            for i in range(output_len):
+                out, (hidden, cn) = self.rnn(self.embedding(dec_input), (hidden,cn))
+                out = self.out(out)  # linear layer, out has now shape [batch, 1, output_size]
+                out = F.log_softmax(out, -1)
+                output.append(out.squeeze(1))
+                out_symbol = torch.argmax(out, dim=2)   # shape [batch, 1]
+                dec_input = out_symbol   # feed the decoded symbol back into the recurrent unit at next step
+
+            output = torch.stack(output).permute(1, 0, 2)  # [batch_size x seq_len x output_size]
+
+        return output
+
+class EncoderRNNMo(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super().__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(input_size, self.hidden_size)
+        rnn = nn.LSTM
+        self.rnn = rnn(self.hidden_size, self.hidden_size, 1, batch_first=True)
+
+    def forward(self, inputs, hidden,cn):
+        # Input shape [batch, seq_in_len]z
+        inputs = inputs.long()
+
+        # Embedded shape [batch, seq_in_len, embed]
+        embedded = self.embedding(inputs)
+        
+        # Output shape [batch, seq_in_len, embed]
+        # Hidden shape [1, batch, embed], last hidden state of the GRU cell
+        # We will feed this last hidden state into the decoder
+        output, (hidden,cn) = self.rnn(embedded, (hidden,cn))
+        #print('ENCODER: output = ',output.shape,'\n ENCODER: hidden = ',hidden.shape)
+        return output, hidden, cn
+
+    def init_hidden(self, batch_size):
+        init = torch.zeros(1, batch_size, self.hidden_size, device=device)
+        cn = torch.zeros(1,batch_size, self.hidden_size, device=device)
+        return init, cn
+
+
+class DecoderRNNMo(nn.Module):
+    def __init__(self, hidden_size, output_size):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+
+        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
+        self.out = nn.Linear(self.hidden_size, self.output_size)
+        rnn = nn.LSTM
+        self.rnn = rnn(self.hidden_size, self.hidden_size, 1, batch_first=True)
+        
+
+        # Attention
+
+        #.W_2 = Parameter(init.kaiming_normal_(torch.Tensor(num_output, num_hidden))) #template
+
+        self.v_a = Parameter(nn.init.kaiming_normal_(torch.Tensor(hidden_size,1)))
+
+        self.W_h = Parameter(nn.init.kaiming_normal_(torch.Tensor(hidden_size, hidden_size)))
+
+        self.W_s = Parameter(nn.init.kaiming_normal_(torch.Tensor(hidden_size, hidden_size)))
+
+        #self.w_c = Parameter(init.kaiming_normal_(torch.Tensor(num_output, num_hidden))) need to be defined
+
+        self.b_alpha = Parameter(nn.init.constant_(torch.Tensor(hidden_size),0))
+
+        self.V_in = nn.Linear(self.hidden_size*2,self.hidden_size,bias=True)
+
+        self.V_out = nn.Linear(self.hidden_size,self.output_size,bias=True)
+        """
+        self.VV_1 = Parameter(nn.init.kaiming_normal_(torch.Tensor(hidden_size*2,output_size)))
+        self.VV_2 = Parameter(nn.init.kaiming_normal_(torch.Tensor(output_size,output_size)))
+        self.bb_1 = Parameter(nn.inin.constant_(torch.Tensor(output_size),0))
+        self.bb_2 = Parameter(nn.inin.constant_(torch.Tensor(output_size),0))
+        """
+
+    def forward(self, inputs, hidden, output_len, cn, encoder_out, teacher_forcing=False):
+        # Input shape: [batch, output_len]
+        # Hidden shape: [seq_len=1, batch_size, hidden_dim] (the last hidden state of the encoder)
+        #print('c_0: {:}'.format(c_0))
+        #print('teacher_forching{:}'.format(teacher_forcing))
+              
+        if teacher_forcing:
+            dec_input = inputs
+            denNyeKonge = torch.zeros(hidden.shape[1],self.output_size,dec_input.shape[1]) # Init the new king. Cheers!
+
+            #print(dec_input)
+            #print('FORWARD: outputsize = ',self.output_size,'\nFORWARD: hiddensize = ',self.hidden_size,'\nFORWARD: decinput = ',dec_input.shape,'\n',dec_input)
+            embedded = self.embedding(dec_input)
+            #print('FORWARD: hidden =',hidden.shape,'\n FORWARD: dennyekonge = ', denNyeKonge.shape,'\n FORWARD: embedded = ',embedded.shape,'\n FORWARD: dec_input = ',dec_input.shape)
+            #out, (hidden, cn) = self.rnn(embedded, (hidden,cn))
+            #print('Skaal!')
+            #print(nyeK)
+            for i in range(dec_input.shape[1]):
+                out, (hidden, cn) = self.rnn(embedded[:,i,...].unsqueeze(1), (hidden,cn))
+                #print('\n FORWARD: out = ',out.shape,'\n FORWARD: encoder_out = ',encoder_out.shape)
+
+                part1 = torch.matmul(encoder_out,self.W_h) # gives the part1 dimension [ B, T, W_h[1] ] since W_h converts from [ d ] to [ W_h[1] ]
+                part2 = torch.matmul(out, self.W_s) # gives the part2 dimension [ B, T, W_s[1] ] since W_s converts from [ d ] to [ W_s[1] ]
+                bjorn = part1 + part2 + self.b_alpha.unsqueeze(0).unsqueeze(0)
+                #print('FORWARD: part1 = ',part1.shape,'\n FORWARD: part2 = ',part2.shape,'\n FORWARD: biazz = ',self.b_alpha.unsqueeze(0).unsqueeze(0).shape)
+                #print('FORWARD: part1 + part2', (part1 + part2.unsqueeze(1)).shape)
+                #print('FORWARD: Bjorn = ',bjorn.shape, '\n FORWARD: tanh(bjorn) = ', F.tanh(bjorn).shape, '\n FORWARD: v_a = ', self.v_a.shape)
+                #print('FORWARD: tanh(bjorn).type = ', F.tanh(bjorn).type)
+                #print('FORWARD: v_a * tanh(bjorn) = ', torch.matmul(F.tanh(bjorn),self.v_a).shape)
+                
+                Cool = F.softmax(torch.matmul(torch.tanh(bjorn),self.v_a),dim=1)
+                #print('FORWARD: cool = ',Cool.shape,'\n FORWARD: cool sq = ',Cool.squeeze(2).shape)
+                
+                #print(Cool)
+                #print(Kewl)
+                #hStar = torch.matmul(encoder_out,Cool)
+                hStar = torch.sum(encoder_out * Cool,dim=1)
+                #print('FORWARD: hStar2 = ',hStar.shape)
+                #Cool = torch.cat((out,Cool))
+                #print('FORWARD: out = ',out.shape)
+
+                tmp = F.log_softmax(self.V_out(self.V_in(torch.cat((out.squeeze(1),hStar),dim=1))),dim=1)
+                #print('FORWARD: tmp = ',tmp.shape,'\n FORWARD: tmp == ',tmp)
+
+                #tmp2 = F.log_softmax(torch.cat((out.squeeze(1),hStar),dim=1))
+
+                denNyeKonge[...,i] = tmp
+
+
+            output = denNyeKonge
+            #out = self.out(denNyeKonge)  # linear layer, out has now shape [batch, output_len, output_size]
+            #output = F.log_softmax(alpha, -1)
+        else:
+            # Take the EOS character only, for the whole batch, and unsqueeze so shape is [batch, 1]
+            # This is the first input, then we will use as input the GRU output at the previous time step
+            dec_input = inputs[:, 0].unsqueeze(1)
+
+            output = []
+            for i in range(output_len):
+                out, (hidden, cn) = self.rnn(self.embedding(dec_input), (hidden,cn))
+                out = self.out(out)  # linear layer, out has now shape [batch, 1, output_size]
+                out = F.log_softmax(out, -1)
+                output.append(out.squeeze(1))
+                out_symbol = torch.argmax(out, dim=2)   # shape [batch, 1]
+                dec_input = out_symbol   # feed the decoded symbol back into the recurrent unit at next step
+
+            output = torch.stack(output).permute(1, 0, 2)  # [batch_size x seq_len x output_size]
+
+        return output
+
+
+
+
+def forward_passNo(encoder, decoder, x, t, t_in, criterion, max_t_len, teacher_forcing):
+    """
+    Executes a forward pass through the whole model.
+    :param encoder:
+    :param decoder:
+    :param x: input to the encoder, shape [batch, seq_in_len]
+    :param t: target output predictions for decoder, shape [batch, seq_t_len]
+    :param criterion: loss function
+    :param max_t_len: maximum target length
+    :return: output (after log-softmax), loss, accuracy (per-symbol)
+    """
+    #print(x.size(),'Et eller andet lort')
+    # Run encoder and get last hidden state (and output)
+    batch_size = x.size(0)
+    enc_h, cn = encoder.init_hidden(batch_size)
+    enc_out, enc_h, cn= encoder(x, enc_h,cn)
+    #print(enc_out.size())
+    dec_h = enc_h  # Init hidden state of decoder as hidden state of encoder
+    dec_input = t_in
+    out = decoder(dec_input, dec_h, max_t_len, cn, teacher_forcing)
+    out = out.permute(0, 2, 1)
+    # Shape: [batch_size x num_classes x out_sequence_len], with second dim containing log probabilities
+
+    loss = criterion(out, t)
+    pred = get_pred(log_probs=out)
+    accuracy = (torch.max((pred == t), (t == 0))).type(torch.FloatTensor).mean()
+    return out, loss, accuracy
+
+def trainNo(encoder, decoder, inputs, targets, targets_in, criterion, enc_optimizer, dec_optimizer, epoch, max_t_len):
+    encoder.train()
+    decoder.train()
+    #batch_size = inputs.size(0)
+    #enc_h = encoder.init_hidden(batch_size)
+
+    #print(inputs[0].size(),targets[0].size(),targets_in[0].size())
+    #print(inputs)
+    for batch_idx, (x, t, t_in) in enumerate(zip(inputs, targets, targets_in)):
+        #print(batch_idx)
+        #print(x.size())    
+        out, loss, accuracy = forward_passNo(encoder, decoder, x, t, t_in, criterion, max_t_len, True)
+            
+        
+        enc_optimizer.zero_grad()
+        dec_optimizer.zero_grad()
+        loss.backward()
+        enc_optimizer.step()
+        dec_optimizer.step()
+        # INSERT YOUR CODE HERE
+        #print(batch_idx)
+        
+        if batch_idx % 50 == 0:
+            encoder.eval()
+            decoder.eval()
+            print('Epoch {} [{}/{} ({:.0f}%)]\tTraining loss: {:.4f} \tTraining accuracy: {:.1f}%'.format(
+                epoch, batch_idx * len(x), TRAINING_SIZE,
+                100. * batch_idx * len(x) / TRAINING_SIZE, loss.item(),
+                100. * accuracy.item()))
+            encoder.train()
+            decoder.train()
+
+
+def testNo(encoder, decoder, inputs, targets, targets_in, criterion, max_t_len):
+    encoder.eval()
+    decoder.eval()
+    with torch.no_grad():
+        inputs = inputs.to(device)
+        targets = targets.long().to(device)
+        targets_in = targets_in.long().to(device)
+        out, loss, accuracy = forward_passNo(encoder, decoder, inputs, targets, targets_in, criterion, max_t_len,
+                                           teacher_forcing=TEACHER_FORCING)
+    return out, loss, accuracy
+
+def trainMo(encoder, decoder, inputs, targets, targets_in, criterion, enc_optimizer, dec_optimizer, epoch, max_t_len):
+    encoder.train()
+    decoder.train()
+    #batch_size = inputs.size(0)
+    #enc_h = encoder.init_hidden(batch_size)
+
+    #print(inputs[0].size(),targets[0].size(),targets_in[0].size())
+    #print(inputs)
+    for batch_idx, (x, t, t_in) in enumerate(zip(inputs, targets, targets_in)):
+        #print(batch_idx)
+        #print(x.size())    
+        out, loss, accuracy = forward_passMo(encoder, decoder, x, t, t_in, criterion, max_t_len, True)
+            
+        
+        enc_optimizer.zero_grad()
+        dec_optimizer.zero_grad()
+        loss.backward()
+        enc_optimizer.step()
+        dec_optimizer.step()
+        # INSERT YOUR CODE HERE
+        #print(batch_idx)
+        
+        if batch_idx % 50 == 0:
+            encoder.eval()
+            decoder.eval()
+            print('Epoch {} [{}/{} ({:.0f}%)]\tTraining loss: {:.4f} \tTraining accuracy: {:.1f}%'.format(
+                epoch, batch_idx * len(x), TRAINING_SIZE,
+                100. * batch_idx * len(x) / TRAINING_SIZE, loss.item(),
+                100. * accuracy.item()))
+            encoder.train()
+            decoder.train()
+
+
+def testMo(encoder, decoder, inputs, targets, targets_in, criterion, max_t_len):
+    encoder.eval()
+    decoder.eval()
+    with torch.no_grad():
+        inputs = inputs.to(device)
+        targets = targets.long().to(device)
+        targets_in = targets_in.long().to(device)
+        out, loss, accuracy = forward_passMo(encoder, decoder, inputs, targets, targets_in, criterion, max_t_len,
+                                           teacher_forcing=TEACHER_FORCING)
+    return out, loss, accuracy
+
+
+def forward_passMo(encoder, decoder, x, t, t_in, criterion, max_t_len, teacher_forcing):
+    """
+    Executes a forward pass through the whole model.
+    :param encoder:
+    :param decoder:
+    :param x: input to the encoder, shape [batch, seq_in_len]
+    :param t: target output predictions for decoder, shape [batch, seq_t_len]
+    :param criterion: loss function
+    :param max_t_len: maximum target length
+    :return: output (after log-softmax), loss, accuracy (per-symbol)
+    """
+    #print('FORWARD: t_in = ',t_in.shape,'\n FORWARD: x = ',x.shape)
+    #print(x.size(),'Et eller andet lort')
+    # Run encoder and get last hidden state (and output)
+    batch_size = x.size(0)
+    enc_h, cn = encoder.init_hidden(batch_size)
+    enc_out, enc_h, cn= encoder(x, enc_h,cn)
+    #print('\n')
+    #print(enc_out.size())
+    #print(enc_h.size())
+    dec_h = enc_h  # Init hidden state of decoder as hidden state of encoder
+    dec_input = t_in
+    out = decoder(dec_input, dec_h, max_t_len, cn, enc_out, teacher_forcing)
+    #print('FORWARD_PASS: out = ',out.shape,'FORWARD_PASS: t = ',t.shape)
+    #print('OUT; ',out)
+    #out = out.permute(0, 2, 1)
+    # Shape: [batch_size x num_classes x out_sequence_len], with second dim containing log probabilities
+    #print(hej)
+    loss = criterion(out, t)
+    pred = get_pred(log_probs=out)
+    accuracy = (torch.max((pred == t), (t == 0))).type(torch.FloatTensor).mean()
+    return out, loss, accuracy
--- variableLength/tmp2.py
+++ variableLength/tmp2.py
@@ -17,18 +17,18 @@ NUM_INPUTS = 27 #No. of possible characters
 NUM_OUTPUTS = 11  # (0-9 + '#')
 
 ### Hyperparameters and general configs
-SEQ_LEN = 150
-MIN_SEQ_LEN = 50
+SEQ_LEN = 40
+MIN_SEQ_LEN = 30
 
 VOCAB = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ']
-ALPHA = 0.3
-MAX_LEN = 10
+ALPHA = 0.35
+MAX_LEN = 5
 
 # Hidden size of enc and dec need to be equal if last hidden of encoder becomes init hidden of decoder
 # Otherwise we would need e.g. a linear layer to map to a space with the correct dimension
 NUM_UNITS_ENC = NUM_UNITS_DEC = 64
 TEST_SIZE = 240
-EPOCHS = 25
+EPOCHS = 50
 
 
 #assert TRAINING_SIZE % BATCH_SIZE == 0
@@ -90,6 +90,9 @@ _,_,t1,t2,t3,lenEnc,lenDec = generateVarLen(SEQ_NUM, VOCAB, ALPHA, MAX_LEN, SEQ_
 Fullval,Shortval,t1val,t2val,t3val,lenEncVal,lenDecVal = generateVarLen(SEQ_NUM, VOCAB, ALPHA, MAX_LEN, SEQ_LEN, MIN_SEQ_LEN)
 #Fullval,Shortval,t1val,t2val,t3val,lenEncVal,lenDecVal = generateMixed(SEQ_NUM, VOCAB, ALPHA,0.5,thresser,MAX_LEN, SEQ_LEN, MIN_SEQ_LEN)
 
+# Get actually validation set
+FullVAL,ShortVAL,t1VAL,t2VAL,t3VAL,lenEncVAL,lenDecVAL = generateVarLen(SEQ_NUM, VOCAB, ALPHA, MAX_LEN, SEQ_LEN, MIN_SEQ_LEN)
+
 #val_inputs = torch.tensor(val_inputs)
 #val_targets = torch.tensor(val_targets)
 #val_targets_in = torch.tensor(val_targets_in)
@@ -126,7 +129,24 @@ lenDec = [lenDec[i * BATCH_SIZE: (i + 1) * BATCH_SIZE] for i in range(TRAINING_S
 
 #print('MAIN: targets = ',inputs[0],'MAIN: lengths = ', lengths[0])
 
+losslist1 = []
+losslist2 = []
+losslist3 = []
 
+acclist1 = []
+acclist2 = []
+acclist3 = []
+
+vlosslist1 = []
+vlosslist2 = []
+vlosslist3 = []
+
+vacclist1 = []
+vacclist2 = []
+vacclist3 = []
+
+
+"""
 for epoch in range(1, EPOCHS + 1):
     train(encoder, decoder, inputs, targets, targets_in, criterion, enc_optimizer, dec_optimizer, epoch, MAX_LEN, lenEnc, lenDec)
     _, loss, accuracy = test(encoder, decoder, t1val, t2val, t3val, criterion, MAX_LEN, lenEncVal, lenDecVal)
@@ -140,5 +160,101 @@ for epoch in range(1, EPOCHS + 1):
     for i in range(numPrintTing):
         print(pred_text[i], "\t", Shortval[i], "\t" ,t2val[i])
     #print()
+"""
+
+
+
+
+
+for epoch in range(1, EPOCHS + 1):
+    trainMo(encoderMo, decoderMo, inputs, targets, targets_in, criterion, enc_optimizerMo, dec_optimizerMo, epoch, SEQ_LEN)
+    _, lossMo, accuracyMo = testMo(encoderMo, decoderMo, t1val, t2val, t3val, criterion, MAX_LEN)
+
+    tmp1 = lossMo.tolist()
+    tmp2 = accuracyMo.tolist()
+
+    losslist2.append(tmp1)
+    acclist2.append(tmp2)
+
+    #if epoch==47:
+    #    print('Epoch 47 has been reached')
+    _, loss2, accuracy2 = testMo(encoderMo, decoderMo, t1VAL, t2VAL, t3VAL, criterion, MAX_LEN)
+    vlosslist2.append(loss2.tolist())
+    vacclist2.append(accuracy2.tolist())
+
+    print('\nTest set - mo attention: Average loss: {:.4f} \tAccuracy: {:.3f}%\n'.format(lossMo, accuracyMo.item()*100.))
+    # Show more examples
+    print("Examples - mo attention: prediction | input")
+    out, _, _ = testMo(encoderMo, decoderMo, t1val[:10], t2val[:10], t3val[:10], criterion, MAX_LEN)
+    pred = get_pred(out)
+    pred_text = [numbers_to_text(sample) for sample in pred]
+    for i in range(10):
+        print(pred_text[i], "\t", Shortval[i], "\t" ,t2val[i])
+
+
+"""
+for epoch in range(1, EPOCHS + 1):
+    train(encoder, decoder, inputs, targets, targets_in, criterion, enc_optimizer, dec_optimizer, epoch, MAX_LEN, lenEnc, lenDec)
+    _, lossMo, accuracyMo = test(encoder, decoder, t1val, t2val, t3val, criterion, MAX_LEN, lenEncVal, lenDecVal)
+
+    tmp1 = lossMo.tolist()
+    tmp2 = accuracyMo.tolist()
+
+    losslist3.append(tmp1)
+    acclist3.append(tmp2)
+
+    #if epoch==47:
+    #    print('Epoch 47 has been reached')
+    _, loss3, accuracy3 = test(encoder, decoder, t1VAL, t2VAL, t3VAL, criterion, MAX_LEN, lenEncVAL, lenDecVAL)
+    vlosslist3.append(loss3.tolist())
+    vacclist3.append(accuracy3.tolist())        
+
+
+    print('\nTest set - var attention: Average loss: {:.4f} \tAccuracy: {:.3f}%\n'.format(lossMo, accuracyMo.item()*100.))
+    # Show more examples
+    print("Examples - var attention: prediction | input")
+    out, _, _ = test(encoder, decoder, t1val[:10], t2val[:10], t3val[:10], criterion, MAX_LEN, lenEncVal[:10], lenDecVal[:10])
+    pred = get_pred(out)
+    pred_text = [numbers_to_text(sample) for sample in pred]
+    for i in range(10):
+        print(pred_text[i], "\t", Shortval[i], "\t" ,t2val[i])
+"""
+
+"""
+for epoch in range(1, EPOCHS + 1):
+    trainNo(encoderNo, decoderNo, inputs, targets, targets_in, criterion, enc_optimizerNo, dec_optimizerNo, epoch, SEQ_LEN)
+    _, lossNo, accuracyNo = testNo(encoderNo, decoderNo, t1val, t2val, t3val, criterion, MAX_LEN)
+    #YOLOSAPIEN
+    tmp1 = lossNo.tolist()
+    tmp2 = accuracyNo.tolist()
+
+    losslist1.append(tmp1)
+    acclist1.append(tmp2)
+    print('\nTest set - no attention: Average loss: {:.4f} \tAccuracy: {:.3f}%\n'.format(lossNo, accuracyNo.item()*100.))
+        
+    _, loss1, accuracy1 = testNo(encoderNo, decoderNo, t1VAL, t2VAL, t3VAL, criterion, MAX_LEN)
+    vlosslist1.append(loss1)
+    vacclist1.append(accuracy1)
+    #if epoch==47:
+    #    print('Epoch 47 has been reached')
+        
+    # Show examples
+    print("Examples - no attention: prediction | input")
+    out, _, _ = testNo(encoderNo, decoderNo, t1val[:10], t2val[:10], t3val[:10], criterion, MAX_LEN)
+    pred = get_pred(out)
+    pred_text = [numbers_to_text(sample) for sample in pred]
+    for i in range(10):
+        print(pred_text[i], "\t", Shortval[i], "\t" ,t2val[i])
+    #print()
+
+    #print()
+"""
+print('Skrald for den specielle: \n',losslist2,'\n',acclist2,'\n',vlosslist2,'\n',vacclist2)
+
+#print('List 1 : \n',losslist1,'\n',acclist1,'\nList 2 : \n',losslist2,'\n',acclist2,'\nList 2 : \n',losslist3,'\n',acclist3,'\n\nValidationerino : \n',loss1,accuracy1,'\n',loss2,accuracy2,'\n',loss3,accuracy3)
+#print('Yolosapiens : \n',losslist3,acclist3,'\n',loss3,accuracy3)
+
+
+
 
 #print(Fullval,Shortval,t1val,t2val)    
